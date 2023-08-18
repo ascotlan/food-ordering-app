3ffd9fed2d77b3c59fd0b134decb8c0ce74c27a2
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-08-18 13:19:28 -0400

    Fixed typo in menu_items table

--- db/schema/05_menu_items.sql
+++ db/schema/05_menu_items.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS menu_items;
+DROP TABLE IF EXISTS menu_items CASCADE;
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
-  restaurant_id INTEGER REFERENCES restaurant(id) ON DELETE CASCADE NOT NULL,
+  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE NOT NULL,
   menu_category_id INTEGER REFERENCES menu_categories(id) ON DELETE CASCADE NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT,
